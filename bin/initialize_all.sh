2e8867f9429fe583cf4a1459b2e52a99e2d12a1b
Steve Chan <sychan168@gmail.com>
2017-11-02 14:48:26 -0700

    Changed to work with dockerize entrypoint on Shock and db_initialize

---
+++ bin/initialize_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# sleep for another 5 seconds to give mysql and mongo more time to come up
+# connect to mysql and load it with mysql data, do the same for mongodb
+#
+
+sleep 5 && \
+echo "Loading mysql data..." && \
+mysql -h ci-mysql -u root -e "source /mnt/mysqldump" && \
+echo "Loading mongodb data..." && \
+mongorestore --host ci-mongo /mnt/ws.mongodump
