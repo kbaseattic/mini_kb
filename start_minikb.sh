11303c5ebd2892a24606aa97fb7effa30d369ae5
Steve Chan <sychan168@gmail.com>
2017-12-20 10:01:09 -0800

    Updated startup script, configs for search, tentative changes for UJS (still broken)

--- start_minikb.sh
+++ start_minikb.sh
@@ -10,16 +10,5 @@
 # 10/17/2017
 #
 
-docker-compose up -d ci-mysql
-docker-compose up -d ci-mongo
-sleep 15 # Give some time for mongo + mysql to come up. This will be unnecessary once we get a
-         # wrapper script that checks for the dependent services to be up before starting
-docker-compose up mongoinit
-docker-compose up mysqlinit
-docker-compose up -d auth2
-docker-compose up -d shock
-sleep 5 # Sometimes shock is a little slow to come up
-docker-compose up -d handle_service
-docker-compose up -d handle_manager
-sleep 5 # Give a little time for the perl apps to come up
-docker-compose up -d workspace
+docker-compose pull
+docker-compose up -d nginx
